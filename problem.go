7c8f187a2d7aef4c2d67355851f57b92783f66e3
pattanachaikhaikham <phattanachai.kh@rmuti.ac.th>
2020-01-27 17:15:31 +0700

    Update problem.go

---
+++ problem.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var studentName [10]string
+	var studentAge [10]int
+	var studentMail [10]string
+
+	studentName[0] = "Goku"
+	studentAge[0] = 18
+	studentMail[0] = "Goku@super.saiya"
+
+	fmt.Println(studentName[0], student[0], studentMail[0])
+}
