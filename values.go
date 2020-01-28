63d2633985c6234dcacab14722a4d67e93a143fb
pattanachaikhaikham <phattanachai.kh@rmuti.ac.th>
2020-01-28 21:41:34 +0700

    Update values.go

--- values.go
+++ values.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+type I interface {
+	F()
+}
+
+func desc(i I) {
+	fmt.Printf("%v,%T\n", i, i)
+}
+
+type T1 struct {
+	text string
+}
+
+func (t T1) F() {
+	fmt.Println(t.text)
+}
+
+type T2 struct {
+	number int
+}
+
+func (t T2) F() {
+	fmt.Println(t.number)
+}
+
+func main() {
+	i1 := T1{"Hello World"}
+	i2 := T2{20}
+	desc(i1)
+	desc(i2)
 }
