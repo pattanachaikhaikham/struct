fe6a17490fe6f8519691cc7a79462b9f24ab8807
pattanachaikhaikham <phattanachai.kh@rmuti.ac.th>
2020-01-28 18:33:34 +0700

    Update struct.go

--- struct.go
+++ struct.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-type student student {
-	name string
-	age int
+type student struct {
+	name  string
+	age   int
 	email string
 }
 
 func main() {
-	 var std [10]student
-	 std[0] = student{"Goku",18,"Goku@super.saiya"}
+	var std [10]student
+	std[0] = student{"Goku", 18, "Goku@super.saiya"}
 
-	 fmt.Println(std[0])
-	 fmt.Println(std[0].name)
+	fmt.Println(std[0])
+	fmt.Println(std[0].name)
 }
