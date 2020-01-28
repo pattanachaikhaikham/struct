e425f64e1de352c4be691b0b7a77c7f40603384a
pattanachaikhaikham <phattanachai.kh@rmuti.ac.th>
2020-01-28 19:31:45 +0700

    Update structsecond.go

--- structsecond.go
+++ structsecond.go
@@ -8,4 +8,11 @@ type student struct {
 
 func main() {
 	var a student
+	one.name = "Goku"
+	one.age = 18
+	one.email = "Goku@super.saiya"
+
+	two := student{"Gohan", 3, "Gohan@super.saiya"}
+
+	three := student{name: "Videl", email: "Videl@daughter.satan"}
 }
